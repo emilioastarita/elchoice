a40b5a980bfc35ef54da2a40a3c24fc97ffd408f
Emilio Astarita <emilio.astarita@gmail.com>
2017-01-24 22:39:13 -0300

    Added prod mode.

--- public/app/main.ts
+++ public/app/main.ts
@@ -1,4 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {AppModule} from "./app.module";
+import {enableProdMode} from "@angular/core";
 
+window['DEPLOY_MODE'] = window['DEPLOY_MODE'] || 'dev';
+console.log('DEPLOY MODE', window['DEPLOY_MODE']);
+if (window['DEPLOY_MODE'] === 'prod') {
+    enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(AppModule);
